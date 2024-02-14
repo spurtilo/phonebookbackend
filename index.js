686a9e51d8587c1e5c3f2f13f0dc0c4fa8bf78d5
Simo Purtilo <148346961+spurtilo@users.noreply.github.com>
2024-02-14 08:55:20 +0200

    Complete exercise 3.9

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const morgan = require("morgan");
+const cors = require("cors");
 const app = express();
 
 app.use(express.json());
+app.use(cors());
 app.use(
   morgan(
     ":method :url :status :res[content-length] - :response-time ms :requestData"
@@ -101,7 +103,7 @@ app.delete("/api/persons/:id", (req, res) => {
   res.status(204).end();
 });
 
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
