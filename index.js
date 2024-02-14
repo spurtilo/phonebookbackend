e11338778f793262d2493206dec8c1ce46bbb66a
Simo Purtilo <148346961+spurtilo@users.noreply.github.com>
2024-02-14 21:08:55 +0200

    Complete exercises 3.13.-3.14

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
+require("dotenv").config();
 const express = require("express");
 const morgan = require("morgan");
 const cors = require("cors");
+const Person = require("./models/person");
 const app = express();
 
 app.use(express.json());
@@ -16,84 +18,43 @@ morgan.token("requestData", (req, res) => {
   return JSON.stringify(req.body);
 });
 
-let persons = [
-  {
-    id: 1,
-    name: "Urho Kekkonen",
-    number: "040 5698 744",
-  },
-  {
-    id: 2,
-    name: "Mauno Koivisto",
-    number: "040 8888 111",
-  },
-  {
-    id: 3,
-    name: "Martti Ahtisaari",
-    number: "050 8756 429",
-  },
-  {
-    id: 4,
-    name: "Tarja Halonen",
-    number: "050 7777 889",
-  },
-  {
-    id: 5,
-    name: "Sauli Niinistö",
-    number: "040 5698 744",
-  },
-];
-
 app.get("/api/persons", (req, res) => {
-  res.json(persons);
+  Person.find({}).then((persons) => {
+    res.json(persons);
+  });
 });
 
 app.get("/api/persons/:id", (req, res) => {
-  const id = Number(req.params.id);
-  const person = persons.find((p) => p.id === id);
-
-  if (person) {
+  Person.findById(req.params.id).then((person) => {
     res.json(person);
-  } else {
-    response.status(404).end();
-  }
+  });
 });
 
 app.get("/info", (req, res) => {
-  res.send(
-    `<p>Phonebook has info for ${persons.length} people</p><br />${new Date()}`
-  );
+  Person.countDocuments({}).then((count) => {
+    res.send(
+      `<p>Phonebook has info for ${count} people</p><br />${new Date()}`
+    );
+  });
 });
-
-const generateId = () => {
-  return Math.floor(Math.random() * 10000);
-};
 
 app.post("/api/persons/", (req, res) => {
   const body = req.body;
 
-  if (!body.name || !body.number) {
+  if (body.name === undefined || body.number === undefined) {
     return res.status(400).json({
       error: "Name or number is missing.",
     });
   }
 
-  if (persons.some((person) => person.name === body.name)) {
-    return res.status(409).json({
-      error: "Name is already in the phonebook.",
-    });
-  }
-
-  const person = {
-    id: generateId(),
+  const person = new Person({
     name: body.name,
     number: body.number,
-  };
+  });
 
-  persons = persons.concat(person);
-  console.log(persons);
-
-  res.json(person);
+  person.save().then((savedPerson) => {
+    res.json(savedPerson);
+  });
 });
 
 app.delete("/api/persons/:id", (req, res) => {
